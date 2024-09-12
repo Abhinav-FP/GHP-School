d0cd354f05eb9be4d1a15ffbc7b807473a4a0c95
sunil-sainiibs37 <sunil.saini@internetbusinesssolutionsindia.com>
2024-09-12 18:43:54 +0530

    last update

--- src/pages/Home/Slider.js
+++ src/pages/Home/Slider.js
@@ -38,49 +38,55 @@ export default function Slider() {
     ];
     return (
         <>
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={1}
-                pagination={{ clickable: true }}
-                autoplay={{ delay: 3000 }} // Slide automatically every 3 seconds
-            >
-                {slides.map((slide, index) => (
-                    <SwiperSlide key={index}>
-                        <div
-                            className="slide-content"
-                            style={{
-                                // backgroundImage: slide.bgImage,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center', 
-                                maxHeight:'700px',
-                                // display: 'flex',
-                                // alignItems: 'center',
-                                // justifyContent: 'center',
-                                color: '#fff',
-                                fontSize: '2rem',
-                                fontWeight: 'bold'
-                            }}
-                        >
-                            <img className='w-full max-h-[700px]' src={slide.bgImage} alt='img' />
-                            <div className="absolute h-full flex items-center left-0 w-full top-1/2 -translate-y-1/2 sliderbg ">
-                                <div className="mx-auto container sm:container md:container lg:max-w-[1232px] px-4">
-                                    <h2 className='merriweather-font font-medium tracking-[-0.04em] text-3xl text-center md:text-4xl lg:text-5xl mb-3'>Welcome to Bal Vishwa Bharti School</h2>
-                                    <p className='max-w-[759px] mx-auto font-medium tracking-[-0.04em]  text-center text-base xl:text-xl'>
-                                        At Bal Vishwa Bharti School, we are committed to nurturing young minds through holistic education, fostering academic excellence, and instilling strong moral values to shape the leaders of tomorrow.
-                                    </p>
+            <div className='sticky'>
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    pagination={{ clickable: true }}
+                    autoplay={{ delay: 3000 }} // Slide automatically every 3 seconds
+                >
+                    {slides.map((slide, index) => (
+                        <SwiperSlide key={index}>
+                            <div
+                                className="slide-content relative"
+                                style={{
+                                    // backgroundImage: slide.bgImage,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                    maxHeight: '700px',
+                                    // display: 'flex',
+                                    // alignItems: 'center',
+                                    // justifyContent: 'center',
+                                    color: '#fff',
+                                    fontSize: '2rem',
+                                    fontWeight: 'bold'
+                                }}
+                            >
+                                <img className='w-full max-h-[700px] object-cover' src={slide.bgImage} alt='img' />
+                                <div className="absolute h-full flex items-center left-0 w-full top-1/2 -translate-y-1/2 sliderbg ">
+                                    <div className="mx-auto container sm:container md:container lg:max-w-[1232px] px-4">
+                                        <h2 className='merriweather-font font-medium tracking-[-0.04em] text-center text-2xl  md:text-4xl lg:text-5xl mb-3'>Welcome to Bal Vishwa Bharti School</h2>
+                                        <p className='max-w-[759px] mx-auto font-medium tracking-[-0.04em]  text-center text-sm md:text-base lg:text-xl'>
+                                            At Bal Vishwa Bharti School, we are committed to nurturing young minds through holistic education, fostering academic excellence, and instilling strong moral values to shape the leaders of tomorrow.
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-            <div className="bg-[#EE834E] flex gap-3">
-                {data && data?.map((item, index) => (
-                    <div className="flex flex-col text-white" key={index}>
-                        <p>{item?.text1}</p>
-                        <p>{item?.text2}</p>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+            <div className="bg-[#EE834E] py-[35px] md:py-[40px] lg:py-[62px]">
+                <div className="mx-auto container sm:container md:container lg:max-w-[1232px] px-4">
+                    <div className='grid gap-5 lg:gap-5 grid-cols-1 sm:grid-cols-2  md:grid-cols-3  lg:grid-cols-5'>
+                        {data && data?.map((item, index) => (
+                            <div className="flex flex-col text-white lg:w-[170px]" key={index}>
+                                <h3 className='text-lg md:text-xl lg:text-2xl merriweather-font tracking-[-0.04em] md:mb-2'>{item?.text1}</h3>
+                                <p className='font-medium tracking-[-0.04em] text-base'>{item?.text2}</p>
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
         </>
     );
