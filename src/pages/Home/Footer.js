4bc56d8a592238c45fb2e8dfe3762ad688d26cf2
sunil-sainiibs37 <sunil.saini@internetbusinesssolutionsindia.com>
2024-09-19 18:23:46 +0530

    academics, about us and facilities

--- src/pages/Home/Footer.js
+++ src/pages/Home/Footer.js
@@ -14,25 +14,25 @@ export default function Footer() {
       <div className='z-[1] relative bg-[#FCFBF4] py-[40px] md:py-[60px] lg:py-[70px] '>
         <div className='container sm:container md:container lg:max-w-[1232px] px-4 mx-auto '>
           <div className='flex flex-wrap -mx-2'>
-            <div className='w-full md:w-7/12 md:w-7/12 px-2'>
-              <h3 className='pb-5 border-b border-black border-opacity-10 text-xl lg:text-2xl merriweather-font font-medium inline-block tracking-[-0.04em] mb-5'>BAL VISHWA BHARTI PUBLIC SR SEC SCHOOL</h3>
+            <div className='w-full lg:w-7/12 px-2 mb-6 lg:mb-0'>
+              <h3 className='pb-3 md:pb-4 lg:pb-5 border-b border-black border-opacity-10 text-xl lg:text-2xl merriweather-font font-medium lg:inline-block tracking-[-0.04em] mb-5'>BAL VISHWA BHARTI PUBLIC SR SEC SCHOOL</h3>
               <div className='space-y-2.5'>
                 <p className='relative pl-6 text-[#1E1E1E] opacity-80 tracking-[-0.04em] text-base font-medium gotham-font'><IoCallOutline className='absolute left-0 top-1/2 -translate-y-1/2' /> 01412282790/ 01412282298/ 90018-69684</p>
                 <p className='relative pl-6 text-[#1E1E1E] opacity-80 tracking-[-0.04em] text-base font-medium gotham-font'><CiMail className='absolute left-0 top-1/2 -translate-y-1/2' /> bvbpschool74@gmail.com/ bvbschool74@gmail.com</p>
                 <p className='relative pl-6 text-[#1E1E1E] opacity-80 tracking-[-0.04em] text-base font-medium gotham-font'><CiLocationOn className='absolute left-0 top-1/2 -translate-y-1/2' /> D-74, Ghiya Marg, Bani Park, Jaipur, Rajasthan 302016</p>
               </div>
             </div>
-            <div className='w-full md:w-3/12 lg:w-2/12 px-2'>
-              <h3 className='pb-5 border-b border-black border-opacity-10 text-xl lg:text-2xl merriweather-font font-medium  tracking-[-0.04em] mb-5'>Quick Links </h3>
+            <div className='w-full md:w-6/12 lg:w-2/12 px-2 mb-6 md:mb-0'>
+              <h3 className='pb-3 md:pb-4 lg:pb-5 border-b border-black border-opacity-10 text-xl lg:text-2xl merriweather-font font-medium  tracking-[-0.04em] mb-5'>Quick Links </h3>
               <div className='space-y-2.5'>
                 <p className='relative pl-6 text-[#1E1E1E] opacity-80 tracking-[-0.04em] text-base font-medium gotham-font'><a href='' className='text-[#1E1E1E] hover:text-[#EE834E]'><IoCallOutline className='absolute left-0 top-1/2 -translate-y-1/2' /> Careers</a></p>
                 <p className='relative pl-6 text-[#1E1E1E] opacity-80 tracking-[-0.04em] text-base font-medium gotham-font'><a href='' className='text-[#1E1E1E] hover:text-[#EE834E]'><BiDonateHeart className='absolute left-0 top-1/2 -translate-y-1/2' /> Sponsor/ Donations</a></p>
                 <p className='relative pl-6 text-[#1E1E1E] opacity-80 tracking-[-0.04em] text-base font-medium gotham-font'><a href='' className='text-[#1E1E1E] hover:text-[#EE834E]'><LuSchool className='absolute left-0 top-1/2 -translate-y-1/2' /> Infrastructure</a></p>
               </div>
             </div>
-            <div className='w-full md:w-3/12 lg:w-3/12 px-2 flex justify-end'>
+            <div className='w-full md:w-6/12 lg:w-3/12 px-2 md:flex md:justify-end'>
               <div>
-                <h3 className='pb-5 border-b border-black border-opacity-10 text-xl lg:text-2xl merriweather-font font-medium  tracking-[-0.04em] mb-5 inline-block'>Follow us on </h3>
+                <h3 className='pb-3 md:pb-4 lg:pb-5 border-b border-black border-opacity-10 text-xl lg:text-2xl merriweather-font font-medium  tracking-[-0.04em] mb-5 lg:inline-block'>Follow us on </h3>
                 <div className='space-y-2.5'>
                   <p className='relative pl-6 text-[#1E1E1E] opacity-80 tracking-[-0.04em] text-base font-medium gotham-font'><a href='' className='text-[#1E1E1E] hover:text-[#EE834E]'><FaFacebook className='absolute left-0 top-1/2 -translate-y-1/2' /> Bal Vishwa Bharti</a></p>
                   <p className='relative pl-6 text-[#1E1E1E] opacity-80 tracking-[-0.04em] text-base font-medium gotham-font'><a href='' className='text-[#1E1E1E] hover:text-[#EE834E]'><IoLogoInstagram className='absolute left-0 top-1/2 -translate-y-1/2' /> @bvbschool</a></p>
--- src/pages/Home/VisionMission.js
+++ src/pages/Home/VisionMission.js
@@ -41,37 +41,38 @@ export default function VisionMission() {
           <div className="flex flex-wrap items-center bg-[#ECE1C5]" key={index}>
             {index % 2 === 0 ? (
               <>
-                <div className="w-full md:w-6/12 p-[30px] lg:p-[40px]">
+                <div className="w-full md:w-6/12 md:order-2">
+
+                  <Image
+                    src={item.imgsrc}
+                    alt={`School Image`}
+                    className="object-cover w-full"
+                  />
+                </div>
+                <div className="w-full md:order-1 md:w-6/12 px-[25px] py-[25px] lg:p-[40px]">
                   <h1 className='merriweather-font font-normal  text-2xl md:text-3xl lg:text-4xl mb-2 text-[#1E1E1E]  tracking-[-0.04em]'>
                     {item?.heading}
                   </h1>
-                  <p className='text-[#666666] text-base font-medium gotham-font tracking-[-0.04em] mb-6 lg:mb-[32px]'>
+                  <p className='text-[#666666] text-base font-medium gotham-font tracking-[-0.04em] mb-5 lg:mb-[32px]'>
                     {item?.text}
                   </p>
 
-                  <button className="bg-[#EE834E] rounded px-8 lg:px-10 py-2 text-white text-base lg:text-lg font-normal tracking-[-0.04em]">
+                  <button className="bg-[#EE834E] hover:bg-[#ECCD6E] rounded px-8 lg:px-10 py-2 text-white text-base lg:text-lg font-normal tracking-[-0.04em]">
                     {item?.btnText}
                   </button>
                 </div>
-                <div className="w-full md:w-6/12 ">
 
-                  <Image
-                    src={item.imgsrc}
-                    alt={`School Image`}
-                    className="object-cover w-full"
-                  />
-                </div>
               </>
             ) : (
               <>
-                <div className="w-full md:w-6/12 ">
+                <div className="w-full md:w-6/12 md:order-1">
                   <Image
                     src={item.imgsrc}
                     alt={`School Image`}
                     className="object-cover w-full"
                   />
                 </div>
-                <div className="w-full md:w-6/12 p-[30px] lg:p-[40px]">
+                <div className="w-full md:w-6/12 p-[30px] lg:p-[40px] md:order-1">
                   <h1 className='merriweather-font font-normal  text-2xl md:text-3xl lg:text-4xl mb-2 text-[#1E1E1E]  tracking-[-0.04em]'>
                     {item?.heading}
                   </h1>
