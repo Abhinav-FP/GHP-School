2093ad64c8cdef9597b511084a3b517cb15af118
sunil-sainiibs37 <sunil.saini@internetbusinesssolutionsindia.com>
2024-09-20 18:31:56 +0530

    new pages

---
+++ src/pages/careers/JoinTeam.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default function ComingSoon() {
+    return (
+        <div className='bg-white pb-[40px] md:pb-[80px] lg:pb-[100px]'>
+            <div className='container sm:container md:container lg:max-w-[1232px] px-4 mx-auto'>
+                <div className='bg-[#ECE1C5]'>
+                    <div className='px-[30px] lg:px-[50px] py-6 lg:py-10 border-b border-black border-opacity-10'>
+                        <h2 class="merriweather-font font-normal text-2xl md:text-3xl lg:text-4xl mb-2.5 text-[#1E1E1E] tracking-[-0.04em] text-center">Infrastructure</h2>
+                    </div>
+                </div>
+                <div className='py-6 lg:py-10 px-[30px] lg:px-[50px]'>
+                     <div className='flex -mx-[15px] flex-wrap'>
+
+                     </div>
+                </div>
+            </div>
+        </div>
+    )
+}
