3816a77742496c52cb6e314fe7e1b7367f797188
Abhinav-FP <a.mathur@futureprofilez.com>
2024-09-21 18:15:40 +0530

    Footer links added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
   return (
 
     <>
+    <Head>
+        <link rel="icon" href="/favicon.png" />
+        <title>BVBS School</title>
+      </Head>
       <Toaster
         toastOptions={{
           position: "top-right",
@@ -14,7 +19,7 @@ export default function App({ Component, pageProps }) {
           },
         }}
       />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
--- src/pages/about/ComingSoon.js
+++ src/pages/about/ComingSoon.js
@@ -9,8 +9,8 @@ export default function ComingSoon() {
       <div className="absolute h-full flex items-center left-0 w-full top-1/2 -translate-y-1/2 sliderbg ">
         <div className="mx-auto container sm:container md:container lg:max-w-[1232px] px-4 text-center">
           <h2 className="capitalize merriweather-font font-normal text-xl sm:text-2xl md:text-3xl lg:text-4xl mb-0 text-[#ECE1C5] tracking-[-0.04em] ">gopal sharma memorial school</h2>
-          <h2 className="uppercase text-[50px] sm:text-[60px]  md:text-[100px]  lg:text-[141px] text-[#E6E6E6] merriweather-font font-normal tracking-[-0.04em]">COMING SOON</h2>
-          <p className="uppercase text-white text-base lg:text-xl">Admissions Open in March 2025</p>
+          {/* <h2 className="uppercase text-[50px] sm:text-[60px]  md:text-[100px]  lg:text-[141px] text-[#E6E6E6] merriweather-font font-normal tracking-[-0.04em]">Coming Soon</h2> */}
+          <p className="uppercase text-white text-base lg:text-xl">Admission Starting Soon</p>
         </div>
       </div>
     </div>   
