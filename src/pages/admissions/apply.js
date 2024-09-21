6d74a929f329a28cdef449986e7e5fca0633ef34
Abhinav-FP <a.mathur@futureprofilez.com>
2024-09-21 11:18:47 +0530

    Corrected routes

---
+++ src/pages/admissions/apply.js
@@ -0,0 +1,18 @@
+import Layout from '@/Component/Layout'
+import React from 'react'
+import ContactForm from './ContactForm'
+
+export default function Apply() {
+  return (
+    <Layout>
+      <div className='bg-no-repeat bg-center bg-cover pt-[102px] md:pt-[152px] md:pt-[195px] pb-[70px]  md:pb-[100px] lg:pb-[132px] lg:h-[500px]  bg-black bg-opacity-40 bg-blend-overlay' style={{ backgroundImage: `url('/Admissions/Banner.png')` }}  >
+        <div className="container sm:container md:container lg:max-w-[1232px] px-4 mx-auto ">
+          <h1 className='capitalize text-white merriweather-font font-normal tracking-[-0.04em] text-center text-3xl md:text-4xl lg:text-5xl mb-0'>
+          Unlock your potential - start your journey with us today
+          </h1>
+        </div>
+      </div> 
+      <ContactForm />
+    </Layout>
+  )
+}
