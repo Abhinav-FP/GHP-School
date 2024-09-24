e92e03fb6777e78e4aea3773d9614f26f7be810e
sunil-sainiibs37 <sunil.saini@internetbusinesssolutionsindia.com>
2024-09-24 13:18:02 +0530

    careers page

--- src/pages/careers/index.js
+++ src/pages/careers/index.js
@@ -2,14 +2,17 @@ import Layout from '@/Component/Layout'
 import React from 'react';
 import CareerBg from '../../../public/Career/careerbg.jpg';
 import Image from 'next/image';
+import JoinTeam from './JoinTeam';
+import Vacancie from './Vacancie';
 
 export default function index() {
   return (
     <Layout>
-      <div className=''  >
+      <div className='w-full'  >
         <Image className='w-full object-cover' height={867} width={1440} src={CareerBg} alt='img' ></Image>
       </div>
-      
+       <JoinTeam />
+       <Vacancie />
     </Layout>
   )
 }
