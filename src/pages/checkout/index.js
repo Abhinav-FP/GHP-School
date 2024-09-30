b3a5b50c6d1bed9df56b99cf46af6fd3ed1c8d8e
sunil-sainiibs37 <sunil-sainiibs37@example.com>
2024-09-30 14:50:12 +0530

    checkout and thankyou page

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,8 +1,126 @@
 import Layout from '@/layout/Layout'
-import React from 'react'
+import React, { useState } from 'react';
+import Image from 'next/image';
 
 export default function index() {
+ 
+      const cartItems = [
+      {
+        Image: "/Facilities/Gallery1.png",
+        Title: "Tuition Fees with Book & Uniform Set",
+        Qty: "X1",
+        Amount: "15000",
+      },
+      {
+        Image: "/Facilities/Gallery1.png",
+        Title: "Tuition Fees with Book & Uniform Set",
+        Qty: "X1",
+        Amount: "15000",
+      },
+      
+    ];
+  
+    // Calculate total price (without considering quantity)
+    const totalPrice = cartItems.reduce((sum, item) => {
+      return sum + Number(item.Amount);
+    }, 0);
+  
   return (
-    <Layout>index</Layout>
+    <Layout>
+      <div className='w-full bg-white py-[50px] md:py-[70px] lg:py-[100px]'>
+        <div className='mx-auto container sm:container md:container lg:max-w-[1232px] px-4'>
+          <div className='flex flex-wrap -mx-4 md:-mx-6 lg:-mx-10'>
+            <div className='w-full lg:w-6/12 px-4 md:px-6 lg:px-10'>
+              <div className="bg-[#ECE1C5]">
+                <div className="px-4 lg:px-[40px] pt-4 lg:pt-[28px] pb-4 lg:pb-[18px]  border-b border-black border-opacity-10">
+                  <h2 className="merriweather-font font-normal  text-2xl md:text-3xl lg:text-4xl mb-0 text-[#1E1E1E]  tracking-[-0.04em]">Sponsor Now</h2>
+                </div>
+                <div className="px-4 lg:px-[40px] py-4 lg:py-[30px] ">
+                  <div className="mb-4 lg:mb-6">
+                    <label className="inline-block text-base text-[#1E1E1E] tracking-[-0.04em] opacity-80 mb-2 lg:mb-2.5 uppercase">full Name<em className='text-[#EE834E]'>*</em> </label>
+                    <input type="text" className="border border-black border-opacity-10 px-3.5 py-2 w-full h-11 lg:h-14 appearance-none h-11 lg:h-[54px] text-[#1E1E1E] tracking-[-0.04em] leading-tight focus:outline-none" />
+                  </div>
+                  <div className="mb-4 lg:mb-6">
+                    <label className="inline-block text-base text-[#1E1E1E] tracking-[-0.04em] opacity-80 mb-2 lg:mb-2.5 uppercase">Contact Number<em className='text-[#EE834E]'>* </em></label>
+                    <input type="text" className="border border-black border-opacity-10 px-3.5 py-2 w-full h-11 lg:h-14 appearance-none h-11 lg:h-[54px] text-[#1E1E1E] tracking-[-0.04em] leading-tight focus:outline-none" />
+                  </div>
+                  <div className="mb-4 lg:mb-6">
+                    <label className="inline-block text-base text-[#1E1E1E] tracking-[-0.04em] opacity-80 mb-2 lg:mb-2.5 uppercase">Adhaar Card<em className='text-[#EE834E]'>*</em> </label>
+                    <input type="file" className="bg-white border border-black border-opacity-10 px-3.5 py-2 w-full h-11 lg:h-14 appearance-none h-11 lg:h-[54px] text-[#1E1E1E] tracking-[-0.04em] leading-tight focus:outline-none" />
+                  </div>
+
+                  <div className="mb-4 lg:mb-6">
+                    <label className="inline-block text-base text-[#1E1E1E] tracking-[-0.04em] opacity-80 mb-2 lg:mb-2.5 uppercase">Pan Card<em className='text-[#EE834E]'>*</em> </label>
+                    <input type="file" className="bg-white border border-black border-opacity-10 px-3.5 py-2 w-full h-11 lg:h-14 appearance-none h-11 lg:h-[54px] text-[#1E1E1E] tracking-[-0.04em] leading-tight focus:outline-none" />
+                  </div>
+                  <div className="mb-4 lg:mb-6">
+                    <label className="inline-block text-base text-[#1E1E1E] tracking-[-0.04em] opacity-80 mb-2 lg:mb-2.5 uppercase">Email address<em className='text-[#EE834E]'>*</em> </label>
+                    <input type="email" className="border border-black border-opacity-10 px-3.5 py-2 w-full h-11 lg:h-14 appearance-none h-11 lg:h-[54px] text-[#1E1E1E] tracking-[-0.04em] leading-tight focus:outline-none" />
+                  </div>
+                  <div className="w-full">
+                    <button type="submit" class="bg-[#EE834E] lg:w-[253px] hover:bg-[#ECCD6E] rounded px-8 lg:px-12 py-2 lg:py-3.5 text-white text-base lg:text-lg font-normal tracking-[-0.04em]" fdprocessedid="1gg5s">Pay Now</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='w-full lg:w-6/12 px-4 md:px-6 lg:px-10'>
+              <div className="pt-4 lg:pt-[28px] pb-4 lg:pb-[18px]  border-b border-black border-opacity-10">
+                <h2 className="merriweather-font font-normal  text-2xl md:text-3xl lg:text-4xl mb-0 text-[#1E1E1E]  tracking-[-0.04em]">Your Sponsorships</h2>
+              </div>
+              <div className="">
+                <table className='w-full'>
+                  <thead>
+                    <tr>
+                      <td className='text-[#1E1E1E] text-base py-3 tracking-[-0.04em] uppercase merriweather-font font-normal border-b border-black border-opacity-10'>
+                        Product
+                      </td>
+                      <td className='text-[#1E1E1E] text-base py-3 px-3 tracking-[-0.04em] uppercase merriweather-font font-normal border-b border-black border-opacity-10'>
+                        QTY
+                      </td>
+                      <td className='text-[#1E1E1E]  text-right text-base py-3 px-3 tracking-[-0.04em] uppercase merriweather-font font-normal border-b border-black border-opacity-10 pr-0'>
+                        Amount
+                      </td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cartItems.map((item, index) =>
+                      <tr key={index}> 
+                        <td  className='text-[#1E1E1E] font-medium text-base py-3.5 tracking-[-0.04em] border-b border-black border-opacity-10'>
+                          <div className='flex items-center '>
+                            <div className='bg-[#E1E1E1] w-[70px] lg:w-[91px]'>
+                              <Image
+                                width={91} height={86}
+                                src={item.Image}
+                                alt={item.Image}
+                                className="object-cover max-w-full"
+                              />
+                            </div>
+                            <div className='w-[calc(100%-71px)] lg:w-[calc(100%-91px)] max-w-[179px] pl-2.5 tracking-[-0.04em] text-[#1E1E1E] font-medium merriweather-font font-normal text-base md:text-lg lg:text-xl'>
+                                {item.Title}
+                            </div>
+                          </div>
+                        </td>
+                        <td className='text-[#1E1E1E] font-medium text-base py-3.5 px-3 tracking-[-0.04em] border-b border-black border-opacity-10'>{item.Qty}</td>
+                        <td className='text-[#EE834E] text-right font-medium text-base py-3.5 px-3 pr-0 tracking-[-0.04em] border-b border-black border-opacity-10'>
+                        ₹ {item.Amount}
+                        </td>
+                      </tr>
+                    )}
+                  </tbody>
+                  <tfoot>
+                     <tr>
+                     <td colSpan={2} className='text-[#1E1E1E] uppercase merriweather-font font-normal text-xl lg:text-2xl py-3.5 tracking-[-0.04em] border-b border-black border-opacity-10'>Total</td>
+                     <td className='text-[#EE834E] text-right py-3.5 px-3 pr-0 tracking-[-0.04em] text-xl lg:text-2xl border-b border-black border-opacity-10 merriweather-font'>
+                      ₹ {totalPrice} 
+                     </td>
+                     </tr>
+                  </tfoot>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
   )
 }
--- src/pages/success/[success].js
+++ src/pages/success/[success].js
@@ -1,8 +1,20 @@
 import Layout from '@/layout/Layout'
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import ThankyouImage from '../../../public/ThankYou/thankyou.png';
 
 export default function Success() {
   return (
- <Layout>success page</Layout>
+    <Layout>
+      <div className='w-full bg-white py-[50px] md:py-[70px] lg:py-[100px]'>
+        <div className='mx-auto container sm:container md:container lg:max-w-[1232px] px-4 text-center'>
+             <Image src={ThankyouImage} className='mx-auto block mb-8 md:mb-10 lg:mb-14'  alt='img'></Image>   
+             <h2 className='merriweather-font font-normal  text-2xl md:text-3xl lg:text-4xl mb-2 text-[#1E1E1E]  tracking-[-0.04em] mb-3 lg:mb-4'>Thank You for Your Generosity!</h2> 
+             <p className='max-w-[708px] mx-auto text-[#666666] text-base font-medium gotham-font tracking-[-0.04em] mb-4 lg:mb-[28px]'>Your donation has been successfully received, and we are incredibly grateful for your support. Your contribution will make a meaningful difference and help us continue our mission.
+             Please check your email for the invoice. To avail tax benefits under Section 80G, kindly download and print a copy of the invoice.</p>
+             <a href='' className='inline-block bg-white hover:bg-[#EE834E] rounded px-6 lg:px-10 py-2 lg:py-2.5 text-[#EE834E] hover:text-white font-normal tracking-[-0.04em] border border-[#EE834E] '>Download Invoice </a>
+        </div>
+      </div>
+    </Layout>
   )
 }
