f919b3ad7cab71d4658b70c921d281bd52bf23c2
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-10-01 15:57:52 +0530

    manage services

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
 // next.config.js
 module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: 'localhost',
-          port: '8000', // Add the port you're using for localhost
-          pathname: '/images/**', // Adjust this path if necessary
-        },
-      ],
-    },
-  }
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'ghp-school-backend.vercel.app',
+        port: '', // Leave empty if not using a specific port
+        pathname: '/images/**', // Adjust this path if necessary
+      },
+      {
+        protocol: 'https', // If your backend supports HTTPS
+        hostname: 'ghp-school-backend.vercel.app',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
+};
