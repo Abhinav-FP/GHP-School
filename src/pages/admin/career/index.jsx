8a5ab5167e85306bf355fc3fca04a770f2c40cf8
Abhinav-FP <a.mathur@futureprofilez.com>
2024-10-02 18:49:05 +0530

    Redploy

--- src/pages/admin/career/index.jsx
+++ src/pages/admin/career/index.jsx
@@ -49,7 +49,7 @@ function Index() {
     }, []);
 
     const handleChange = (e) => {
-        const { name, value } = e.target;
+        const { name, value } = e.target; 
         setFormData((prevData) => ({
             ...prevData,
             [name]: value
