2258ca64e000bcd2a8a5d84f1ac3e6073417bd23
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-10-10 10:44:32 +0530

    manage all  new Api implenetion

---
+++ src/pages/admin/Component/Image.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+// import dataimage from "../../../public/assets/images/bydefault.png"
+export default function Image({alt, src, classes}) {
+
+    const [imageSRC, setImageSRC]  =  useState(src);
+    const imageLoader = () => { 
+      setImageSRC(dataimage);
+    }
+    return (
+        <img
+          className={`card-img-top img-fluid mr-2 ${classes}`}
+          onError={imageLoader}
+          src={imageSRC || dataimage}
+          alt={alt}
+        />
+    )
+}
