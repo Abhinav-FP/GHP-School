947eddf4c44bbc29f061f1e1e69c8ebca63829ad
sunil-sainiibs37 <sunil-sainiibs37@example.com>
2024-10-21 18:01:48 +0530

    last changes

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -6,26 +6,31 @@ export default function NotFound() {
 
   return (
     <Layout>
-    <div className="flex flex-col items-center justify-center">
-      {/* <h1 className="text-6xl font-bold text-orange-600 mb-4">404</h1> */}
-      <div className="flex flex-col items-center">
-        <img
-          src="/Home/Image404.png" // Replace this with your image URL
-          alt="Page Not Found"
-          className="mb-4"
-        />
-        <h2 className="text-[36px] text-[#1E1E1E] font-semibold mb-2">Page Not Found</h2>
-        <p className="text-gray-500 text-center mb-6">
-          Sorry, the page you are looking for is not available.
-        </p>
-        <Link
-          href="/"
-          className="px-6 py-2 text-[#EE834E] border-2 border-[#EE834E]"
-        >
-          Back to Home
-        </Link>
+      <div className='bg-white py-[50px] md:py-[60px] lg:py-[80px]'>
+        <div className='container sm:container md:container lg:max-w-[1204px] px-4 mx-auto'>
+          <div className="flex flex-col items-center justify-center">
+            {/* <h1 className="text-6xl font-bold text-orange-600 mb-4">404</h1> */}
+            <div className="flex flex-col items-center">
+              <img
+                src="/Home/Image404.png" // Replace this with your image URL
+                alt="Page Not Found"
+                className="mb-6 md:mb-[40px] lg:mb-[58px]"
+              />
+              <h2 className="merriweather-font font-normal  text-2xl md:text-3xl lg:text-4xl mb-2 text-[#1E1E1E]  tracking-[-0.04em]">Page Not Found</h2>
+              <p className="text-[#666666] font-medium text-base  gotham-font text-center mb-1.5 tracking-[-0.04em]  text-center mb-6 md:mb-[30px] lg:mb-[40px]">
+                Sorry, the page you are looking for is not available.
+              </p>
+              <Link
+                href="/"
+                className="block text-[#EE834E] button-animation-border hover:text-white border border-[#EE834E] hover:border-[#ECCD6E]  text-base lg:text-lg rounded px-6 md:px-12 py-2 text-center tracking-[-0.04em]"
+              >
+                Back to Home
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+
     </Layout>
   );
 }
