1311d9249ed92073807c0ab5ac46b03b21706770
sunil-sainiibs37 <sunil-sainiibs37@example.com>
2024-10-21 17:14:40 +0530

    last changes

--- src/pages/academics/BoardDetail.js
+++ src/pages/academics/BoardDetail.js
@@ -8,8 +8,8 @@ export default function index() {
       <div className="bg-white py-[40px] md:py-[80px] lg:py-[100px]" id="board">
         <div className="container sm:container md:container lg:max-w-[1204px] px-4 mx-auto ">
           <div className="border-t border-b border-black border-opacity-10 py-[45px]">
-            <div className="flex flex-wrap -mx-[30px] items-center">
-              <div className="w-full md:w-6/12 px-[30px] text-center border-r border-black border-opacity-10 py-[26px]">
+            <div className="flex flex-wrap -mx-4 md:-mx-[20px] lg:-mx-[30px] items-center">
+              <div className="w-full md:w-6/12 px-4 md:px-[20px] lg:px-[30px] text-center border-r border-black border-opacity-10 py-[16px] md:py-[26px]">
                 <Image
                   blurDataURL={`${BoradLogo}?q=1`}
                   placeholder="blur"
@@ -22,8 +22,8 @@ export default function index() {
                   Rajasthan Board
                 </p>
               </div>
-              <div className="w-full md:w-6/12 px-[30px] ">
-                <p className="tracking-[-0.04em] text-[#666666] text-2xl merriweather-font ">
+              <div className="w-full md:w-6/12 px-4 md:px-[20px] lg:px-[30px] ">
+                <p className="tracking-[-0.04em] text-[#666666] text-lg lg:text-2xl merriweather-font ">
                   At Bal Vishwa Bharti School, we provide high-quality education
                   from Nursery to Grade XII, following the Rajasthan Board of
                   Secondary Education (RBSE) curriculum. Our English Medium
