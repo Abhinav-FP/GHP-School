ca8628265b1af01cb6c68c411bca7b7f3d71d459
Abhinav-FP <a.mathur@futureprofilez.com>
2024-11-04 11:17:41 +0530

    Checking error again 5

--- src/pages/admin/banner/index.jsx
+++ src/pages/admin/banner/index.jsx
@@ -94,7 +94,7 @@ function Banner() {
         setError(true);
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-      console.log("response?.data",response?.data);
+      console.log("response?.data",response);
       if (response?.data?.success !== true) {
         console.log("Error in success line");
         setImageUploading(false);
