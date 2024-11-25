6448097d31522bab397aeba827ddd22a3a3b5211
Abhinav-FP <a.mathur@futureprofilez.com>
2024-11-25 14:27:54 +0530

    Email changed

--- src/pages/contact/GetTouch.js
+++ src/pages/contact/GetTouch.js
@@ -6,7 +6,7 @@ import { CiLocationOn } from "react-icons/ci";
 function GetTouch() {
   return (
     <div className="bg-white py-[50px] md:py-[70px] lg:py-[100px]" id="touch">
-      <div className="container sm:container md:container lg:max-w-[1204px] px-4 mx-auto" >
+      <div className="container sm:container md:container lg:max-w-[1204px] px-4 mx-auto">
         <div className="flex flex-wrap items-center -mx-4">
           <div className="w-full lg:w-6/12 px-4 mb-6 lg:mb-0">
             <h2 className="merriweather-font font-normal  text-2xl md:text-3xl lg:text-4xl mb-5 md:mb-6 lg:mb-8 text-[#1E1E1E]  tracking-[-0.04em]">
@@ -31,10 +31,10 @@ function GetTouch() {
                   size={18}
                 />
                 <a href="mailto:bvbpschool74@gmail.com">
-                  bvbpschool74@gmail.com
+                bvbpschool74@gmail.com
                 </a>
                 /{" "}
-                <a href="mailto:bvbschool74@gmail.com">bvbschool74@gmail.com</a>
+                <a href="mailto:bvbpschool@yahoo.com">bvbpschool@yahoo.com</a>
               </p>
               <p className="relative pl-6 text-[#1E1E1E] opacity-80 tracking-[-0.04em] text-base font-medium gotham-font">
                 <a href="https://www.google.com/maps/dir//D,+74,+Ghiya+Marg,+Sindhi+Colony,+Bani+Park,+Jaipur,+Rajasthan+302032+@26.931568,75.791982/@27.7436422,76.5794786,7z/data=!4m7!4m6!1m1!4e2!1m2!1m1!1s0x396db3efafa594ef:0x2da3b3ab79a793f4!3e0">
@@ -48,17 +48,16 @@ function GetTouch() {
             </div>
           </div>
           <div className="w-full lg:w-6/12 px-4">
-  <div className="relative pb-[56.25%] h-0 overflow-hidden">
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d222.31967111126167!2d75.79197175989164!3d26.931576820166637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d26.931562!2d75.7920073!4m5!1s0x396db3efafa594ef%3A0x2da3b3ab79a793f4!2sD%2C%2074%2C%20Ghiya%20Marg%2C%20Sindhi%20Colony%2C%20Bani%20Park%2C%20Jaipur%2C%20Rajasthan%20302032!3m2!1d26.9315642!2d75.7919816!5e0!3m2!1sen!2sin!4v1727153589766!5m2!1sen!2sin"
-      className="absolute top-0 left-0 w-full h-full border-0"
-      allowFullScreen
-      loading="lazy"
-      referrerPolicy="no-referrer-when-downgrade"
-    ></iframe>
-  </div>
-</div>
-
+            <div className="relative pb-[56.25%] h-0 overflow-hidden">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d222.31967111126167!2d75.79197175989164!3d26.931576820166637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d26.931562!2d75.7920073!4m5!1s0x396db3efafa594ef%3A0x2da3b3ab79a793f4!2sD%2C%2074%2C%20Ghiya%20Marg%2C%20Sindhi%20Colony%2C%20Bani%20Park%2C%20Jaipur%2C%20Rajasthan%20302032!3m2!1d26.9315642!2d75.7919816!5e0!3m2!1sen!2sin!4v1727153589766!5m2!1sen!2sin"
+                className="absolute top-0 left-0 w-full h-full border-0"
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+          </div>
         </div>
       </div>
     </div>
